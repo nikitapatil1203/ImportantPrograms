7a9462989304f44757284cdf0fc7096fca5aa18c
Nikita Patil <nikitapatil@Nikitas-MacBook-Air.local>
2025-06-12 20:54:51 +0530

    new code

--- src/FindTwoStringsAreAnagramOrNot.java
+++ src/FindTwoStringsAreAnagramOrNot.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class FindTwoStringsAreAnagramOrNot {
 
@@ -6,7 +8,28 @@ public class FindTwoStringsAreAnagramOrNot {
 		String str = "listen";
 		String str2  = "silent";
 		
+		List<Character> lst = new ArrayList<Character>();
+		List<Character> lst1 = new ArrayList<Character>();
 		
+		for(int i=0;i<=str.length()-1;i++)
+		{
+			lst.add(str.charAt(i));
+		}
+		
+		for(int j=0;j<=str2.length()-1;j++)
+		{
+			lst1.add(str2.charAt(j));
+		}
+		System.out.println(lst);
+		System.out.println(lst1);
+		if(lst.containsAll(lst1))
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
 	}
 
 }
